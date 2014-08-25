2b0b4a0cad47faa94f2d4d7f9a0bf7dfec67f73a
WesleyGo <marc.wesley.go@gmail.com>
2014-08-25 19:36:30 +0800

    new changes

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ var express = require('express'),
  
 var app = express();
 
-app.use(bodyParser())
+app.use(bodyParser());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS");
+  next();
+});
  
 app.get('/wines', wine.findAll);
 app.get('/wines/:id', wine.findById);
